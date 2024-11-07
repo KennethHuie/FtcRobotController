375ee7add3a2be20678f50c5c01499981ff664f9
KennethHuie <s_kenneth.huie@ousd.org>
2024-11-06 18:19:12 -0800

    s

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Utilities/Slider_Experiment11_4_2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Utilities/Slider_Experiment11_4_2024.java
@@ -8,8 +8,7 @@ public class Slider_Experiment11_4_2024 extends LinearOpMode {
         DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "drive");
         waitForStart();
         while (opModeIsActive()) {
-            motor.setPower(gamepad1.right_trigger);
-            motor.setPower(-gamepad1.left_trigger);
+            motor.setPower(gamepad1.right_trigger-gamepad1.left_trigger);
             telemetry.addData("Current Power",motor.getPower());
             telemetry.update();
         }
