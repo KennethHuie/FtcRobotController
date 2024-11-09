4ffea2d130d0d9d4391640d281ef01b517a38476
KennethHuie <s_kenneth.huie@ousd.org>
2024-11-08 21:54:21 -0800

    Testrig TeleOp made Added WheelConfigDebug TeleOp to aid debugging Un-hardcoded MecanumBase Smarter Configuration format (still needs some cleaning up) Gradle Updated Changed some TeleOp groups

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Utilities/ServoContinuous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Utilities/ServoContinuous.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.teamcode.Utilities;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "servoContinuous",group = "Utilities")
 public class ServoContinuous extends LinearOpMode {
