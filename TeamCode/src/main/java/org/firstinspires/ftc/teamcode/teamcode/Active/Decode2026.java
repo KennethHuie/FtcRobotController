5dc0a068b0fda5b6753dcd98f7abafaf5fc457fa
KennethHuie <s_kenneth.huie@ousd.org>
2026-03-26 17:12:12 -0700

    panels control support + disable i2c devices

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Active/Decode2026.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Active/Decode2026.java
@@ -2,18 +2,20 @@ package org.firstinspires.ftc.teamcode.teamcode.Active;
 
 import android.annotation.SuppressLint;
 
+import com.bylazar.gamepad.GamepadManager;
+import com.bylazar.gamepad.PanelsGamepad;
 import com.qualcomm.ftccommon.SoundPlayer;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.teamcode.Configuration;
 import org.firstinspires.ftc.teamcode.teamcode.MecanumBase;
@@ -24,6 +26,8 @@ import java.util.Locale;
 @TeleOp(name = "Decode2026 (AndroidStudio)", group = "Active")
 public class Decode2026 extends LinearOpMode {
     Configuration.Decode2026 cfg = new Configuration.Decode2026();
+
+    GamepadManager gpm1 = PanelsGamepad.INSTANCE.getFirstManager(); // Bylazar panels
 
     private final ElapsedTime runtime = new ElapsedTime(); //Time since startup
     private final double lastElapsed = runtime.milliseconds();
@@ -55,7 +59,8 @@ public class Decode2026 extends LinearOpMode {
         boolean _reverse = false;// Debounce
         MecanumBase mbs = new MecanumBase(hardwareMap, cfg, telemetry);
 
-        int flywheelSpeed = 20;
+        int flywheelSpeed = 25;
+
         boolean readyPlayed = false; // Used to rumble controller when flywheel speed target hit
 
         @SuppressLint("DiscouragedApi") final int forwardModeID = hardwareMap.appContext.getResources().getIdentifier("forwardmode", "raw", hardwareMap.appContext.getPackageName());
@@ -99,9 +104,10 @@ public class Decode2026 extends LinearOpMode {
         waitForStart();
         runtime.reset();
         while (opModeIsActive()) {
+            Gamepad Gamepad1 = gpm1.asCombinedFTCGamepad(gamepad1);
             // Drive time scaling
             double timeScaleDrive;
-            if (Math.abs(gamepad1.left_stick_y) == 0) lastTimeDrive = runtime.milliseconds();
+            if (Math.abs(Gamepad1.left_stick_y) == 0) lastTimeDrive = runtime.milliseconds();
 
             timeScaleDrive = (runtime.milliseconds() - lastTimeDrive) / cfg.timeToMaxScale;
             timeScaleDrive = Range.clip(timeScaleDrive, 0, 1);
@@ -111,7 +117,7 @@ public class Decode2026 extends LinearOpMode {
 
             // Strafe time scaling
             double timeScaleStrafe;
-            if (Math.abs(gamepad1.left_stick_x) == 0) {
+            if (Math.abs(Gamepad1.left_stick_x) == 0) {
                 lastTimeStrafe = runtime.milliseconds();
             }
 
@@ -120,12 +126,12 @@ public class Decode2026 extends LinearOpMode {
             timeScaleStrafe = Range.clip(timeScaleStrafe, 0, 1);
 
             //Movement variables, all clamped
-            double drive = Range.clip(-gamepad1.left_stick_y * cfg.scaleDrive, -cfg.maxDrive, cfg.maxDrive) * timeScaleDrive;
-            double turn = Range.clip(gamepad1.right_stick_x * cfg.scaleTurn, -cfg.maxTurn, cfg.maxTurn) * timeScaleTurn;
-            double strafe = Range.clip(gamepad1.left_stick_x * cfg.scaleStrafe, -cfg.maxStrafe, cfg.maxStrafe) * timeScaleStrafe;
+            double drive = Range.clip(-Gamepad1.left_stick_y * cfg.scaleDrive, -cfg.maxDrive, cfg.maxDrive) * timeScaleDrive;
+            double turn = Range.clip(Gamepad1.right_stick_x * cfg.scaleTurn, -cfg.maxTurn, cfg.maxTurn) * timeScaleTurn;
+            double strafe = Range.clip(Gamepad1.left_stick_x * cfg.scaleStrafe, -cfg.maxStrafe, cfg.maxStrafe) * timeScaleStrafe;
 
             // Reverse driving mode logic
-            if (!gamepad1.y) _reverse = false;
+            if (!Gamepad1.y) _reverse = false;
             if (reverse) {
                 drive = -drive;
                 strafe = -strafe;
@@ -134,7 +140,7 @@ public class Decode2026 extends LinearOpMode {
             //Send control values to the basic Mecanum Drivetrain
             mbs.setPower(drive, turn, -strafe);
             // Reverse controls with toggle button
-            if (gamepad1.y) {
+            if (Gamepad1.y) {
                 if (!_reverse) {
                     _reverse = true;
                     reverse = !reverse;
@@ -148,10 +154,10 @@ public class Decode2026 extends LinearOpMode {
             }
 
             // Feed/Sweep up system
-            sweeper.setPower(gamepad1.right_bumper ? 1 : gamepad1.left_bumper ? -1 : 0);
+            sweeper.setPower(Gamepad1.right_bumper ? 1 : Gamepad1.left_bumper ? -1 : 0);
             //Turret
-            double leftPower = circleCurve(gamepad1.left_trigger);
-            double rightPower = circleCurve(gamepad1.right_trigger);
+            double leftPower = circleCurve(Gamepad1.left_trigger);
+            double rightPower = circleCurve(Gamepad1.right_trigger);
             if (turret.getCurrentPosition() > 1000) {
                 rightPower = 0;
             }
@@ -160,7 +166,7 @@ public class Decode2026 extends LinearOpMode {
             }
             turret.setPower(rightPower - leftPower);
 
-            intakeBlocker.setPosition(gamepad1.a ? 1 : -1);
+            intakeBlocker.setPosition(Gamepad1.a ? 1 : -1);
 
             // Flywheel
             int delta1 = flywheel1.getCurrentPosition() - lastFly1;
@@ -172,19 +178,19 @@ public class Decode2026 extends LinearOpMode {
             if (Math.abs(delta1) > flywheelSpeed && Math.abs(delta2) > flywheelSpeed) { // Play sound when flywheels are ready
                 if (!readyPlayed) {
                     readyPlayed = true;
-                    gamepad1.rumble(500);
+                    Gamepad1.rumble(500);
                 }
-                flywheel1.setPower(gamepad1.a ? 0.35 : 0);
-                flywheel2.setPower(gamepad1.a ? 0.35 : 0);
+                flywheel1.setPower(Gamepad1.a ? 0.35 : 0);
+                flywheel2.setPower(Gamepad1.a ? 0.35 : 0);
             } else {
                 double power = ((flywheelSpeed - (Math.abs((double) delta1) + Math.abs((double) delta2)) / 2) / 10) + 0.3;
-                flywheel1.setPower(gamepad1.a ? power : 0);
-                flywheel2.setPower(gamepad1.a ? power : 0);
+                flywheel1.setPower(Gamepad1.a ? power : 0);
+                flywheel2.setPower(Gamepad1.a ? power : 0);
                 readyPlayed = false;
             }
 
             // Toggle kickstand servos
-            if (gamepad1.x) {
+            if (Gamepad1.x) {
                 if (!standLeft.getDebounce()) {
                     standLeft.setState(!standLeft.getState());
                     standLeft.setDebounce(true);
@@ -195,19 +201,19 @@ public class Decode2026 extends LinearOpMode {
                 standLeft.setDebounce(false);
             }
 
-            if (gamepad1.dpad_up) {
+            if (Gamepad1.dpad_up) {
                 hoodLeft.setPosition(1);
                 hoodRight.setPosition(1);
             }
-            if (gamepad1.dpad_down) {
+            if (Gamepad1.dpad_down) {
                 hoodLeft.setPosition(-1);
                 hoodRight.setPosition(-1);
             }
 
-            if (gamepad1.dpadRightWasPressed()) {
+            if (Gamepad1.dpadRightWasPressed()) {
                 flywheelSpeed += 1;
             }
-            if (gamepad1.dpadLeftWasPressed()) {
+            if (Gamepad1.dpadLeftWasPressed()) {
                 flywheelSpeed -= 1;
             }
 
@@ -224,10 +230,10 @@ public class Decode2026 extends LinearOpMode {
             telemetry.addLine();
 //            telemetry.addData("LD", leftDistanceSensor.getDistance(DistanceUnit.CM));
 //            telemetry.addData("RD", rightDistanceSensor.getDistance(DistanceUnit.CM));
-            NormalizedRGBA rcs = roofColorSensor.getNormalizedColors();
-            String something = String.format(Locale.ENGLISH, "%f,%f,%f", rcs.red, rcs.green, rcs.blue);
-            telemetry.addData("RC", something);
-            telemetry.addData("Rangefinder", turretRangefinder.getDistance(DistanceUnit.CM));
+//            NormalizedRGBA rcs = roofColorSensor.getNormalizedColors();
+//            String something = String.format(Locale.ENGLISH, "%f,%f,%f", rcs.red, rcs.green, rcs.blue);
+//            telemetry.addData("RC", something);
+//            telemetry.addData("Rangefinder", turretRangefinder.getDistance(DistanceUnit.CM));
 //            telemetry.addData("BC", rearColorSensor.getNormalizedColors().toColor());
             telemetry.update();
         }
