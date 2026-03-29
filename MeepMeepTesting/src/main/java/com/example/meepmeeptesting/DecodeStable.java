34cf2054b17d91f1406570d1ab72df2facffd138
KennethHuie <s_kenneth.huie@ousd.org>
2026-03-28 17:42:48 -0700

    MeepMeep layouts for initial auton plan

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/DecodeStable.java
@@ -0,0 +1,41 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class DecodeStable {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(1080);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(40, 25, Math.toRadians(180), Math.toRadians(180), 18)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -16,Math.toRadians(180)))
+                .strafeToLinearHeading(new Vector2d(0,0),Math.toRadians(90))
+                .waitSeconds(5)
+                .strafeTo(new Vector2d(-12,30))
+                .strafeTo(new Vector2d(-12,45))
+                .strafeTo(new Vector2d(0,0))
+                .waitSeconds(5)
+                .strafeTo(new Vector2d(12,30))
+                .strafeTo(new Vector2d(12,45))
+                .strafeTo(new Vector2d(0,0))
+                .waitSeconds(5)
+                .strafeTo(new Vector2d(34,30))
+                .strafeTo(new Vector2d(34,45))
+                .strafeTo(new Vector2d(0,0))
+                .waitSeconds(5)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
